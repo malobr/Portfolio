464cff1f8643b9ac7ecd46299c18031111b363c0
malobr <marcelotc1202@gmail.com>
2026-04-23 21:32:43 -0300

    Adicionando Project e POst Seeder no DatabaseSeeder

--- Portfolio/database/seeders/DatabaseSeeder.php
+++ Portfolio/database/seeders/DatabaseSeeder.php
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminSeeder::class,
             PostSeeder::class,
+            ProjectSeeder::class,
+            PostSeeder::class,
         ]);
     }
 }
